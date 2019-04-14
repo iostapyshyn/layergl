91c3cb43d73eebfdde6adaee95c47ea5c738e3ab
Ilya Ostapyshyn <ilya.ostapyshyn@gmail.com>
2019-04-14 12:19:21 +0200

    new useful "Rect" structure

--- geometry.go
+++ geometry.go
@@ -22,6 +22,23 @@ type VertexObject struct {
 	Indices  []int
 }
 
+// Return distance between two points.
+func Distance(a, b Point) float64 {
+	return math.Sqrt((b.X-a.X)*(b.X-a.X) + (b.Y-a.Y)*(b.Y-a.Y))
+}
+
+// Returns geometrical center, center of mass (centroid) of a polygon.
+func (v VertexObject) Centroid() (center Point) {
+	for _, p := range v.Vertices {
+		center.X += p.X
+		center.Y += p.Y
+	}
+
+	center.X /= float64(len(v.Vertices))
+	center.Y /= float64(len(v.Vertices))
+	return center
+}
+
 func Rectangle(rect Rect) (polygon *VertexObject) {
 	polygon = new(VertexObject)
 	polygon.Vertices = []Point{
@@ -115,4 +132,54 @@ func (v *VertexObject) Scale(scale float64) {
 		v.Vertices[i].X += center.X
 		v.Vertices[i].Y += center.Y
 	}
+}
+
+// Return rectangle approximating bounds of the polygon.
+func (v VertexObject) Bounds() Rect {
+	if !(len(v.Vertices) > 0) {
+		return Rect{}
+	}
+
+	xmost := math.Inf(-1)
+	ymost := math.Inf(-1)
+	xleast := math.Inf(+1)
+	yleast := math.Inf(+1)
+
+	for _, p := range v.Vertices {
+		if p.X > xmost {
+			xmost = p.X
+		}
+		if p.Y > ymost {
+			ymost = p.Y
+		}
+		if p.X < xleast {
+			xleast = p.X
+		}
+		if p.Y < yleast {
+			yleast = p.Y
+		}
+	}
+
+	return Rect{xleast, yleast, xmost, ymost}
+}
+
+func (rect Rect) Contains(p Point) bool {
+	return p.X > rect.X1 && p.X < rect.X2 && p.Y > rect.Y1 && p.Y < rect.Y2
+}
+
+func (rect Rect) Intersects(r2 Rect) bool {
+	return rect.Contains(Point{r2.X1, r2.Y1}) || rect.Contains(Point{r2.X2, r2.Y2}) ||
+		rect.Contains(Point{r2.X1, r2.Y2}) || rect.Contains(Point{r2.X2, r2.Y1})
+}
+
+func (rect Rect) Height() float64 {
+	return rect.Y2 - rect.Y1
+}
+
+func (rect Rect) Width() float64 {
+	return rect.X2 - rect.X1
+}
+
+func (rect Rect) Area() float64 {
+	return rect.Height() * rect.Width()
 }
